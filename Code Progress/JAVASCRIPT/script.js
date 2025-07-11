4f62c0da2d582670eeea9506cb38823612632260
Vincent <154762250+Vncn8@users.noreply.github.com>
2025-07-11 20:26:59 +0800

    Create script.js

---
+++ Code Progress/JAVASCRIPT/script.js
@@ -0,0 +1,27 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+function revealOnScroll() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+revealOnScroll();
